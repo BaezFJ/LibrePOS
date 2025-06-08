5433a172395a88b0d2bd097fd13601a403903eec
BaezFJ <baezdevs@gmail.com>
2025-06-08 13:40:53 -0500

    Export repository classes in `__init__.py` for streamlined imports

---
+++ librepos/repositories/__init__.py
@@ -0,0 +1,11 @@
+from .entity_repo import EntityRepository
+from .menu_category_repo import MenuCategoryRepository
+from .menu_group_repo import MenuGroupRepository
+from .user_repository import UserRepository
+
+__all__ = [
+    "EntityRepository",
+    "MenuCategoryRepository",
+    "MenuGroupRepository",
+    "UserRepository",
+]
