3be9f15fde681c647e0376935df5b81fe2ffbb63
BaezFJ <baezdevs@gmail.com>
2025-06-08 13:41:23 -0500

    Refactor `UserRepository` to extend `EntityRepository` for streamlined CRUD functionality and code reuse.

--- librepos/repositories/user_repository.py
+++ librepos/repositories/user_repository.py
@@ -1,35 +1,11 @@
-from librepos.extensions import db
 from librepos.models.users import User
+from .entity_repo import EntityRepository
 
 
-class UserRepository:
-    @staticmethod
-    def get_by_id(user_id: int) -> User | None:
-        return User.query.get(user_id)
-
-    @staticmethod
-    def get_all() -> list[User]:
-        return User.query.all()
+class UserRepository(EntityRepository[User]):
+    def __init__(self):
+        super().__init__(User)
 
     @staticmethod
     def find_by_email(email: str) -> User | None:
         return User.query.filter_by(email=email).first()
-
-    @staticmethod
-    def create(data) -> User:
-        user = User(**data)
-        db.session.add(user)
-        db.session.commit()
-        return user
-
-    @staticmethod
-    def update(user: User, **data) -> User:
-        for key, val in data.items():
-            setattr(user, key, val)
-        db.session.commit()
-        return user
-
-    @staticmethod
-    def delete(user: User) -> None:
-        db.session.delete(user)
-        db.session.commit()
