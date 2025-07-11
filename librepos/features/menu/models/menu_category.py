bbad47cc2391eeb3e2005837b8c2f632100871ac
BaezFJ <baezdevs@gmail.com>
2025-07-11 14:07:46 -0500

    Refactor `MenuCategory` model: add timestamps, user tracking fields, and optional description; update relationships and column definitions.

--- librepos/features/menu/models/menu_category.py
+++ librepos/features/menu/models/menu_category.py
@@ -1,9 +1,11 @@
-from typing import List, TYPE_CHECKING
+from datetime import datetime
+from typing import List, TYPE_CHECKING, Optional
 
+from sqlalchemy import ForeignKey
 from sqlalchemy.orm import Mapped, relationship, mapped_column
 
 from librepos.extensions import db
-from librepos.utils import slugify_string
+from librepos.utils import timezone_aware_datetime
 
 if TYPE_CHECKING:
     from .menu_group import MenuGroup
@@ -17,13 +19,21 @@ class MenuCategory(db.Model):
     def __init__(self, name: str, **kwargs):
         super(MenuCategory, self).__init__(**kwargs)
         self.name = name.capitalize()
-        self.slug = slugify_string(name)
+        self.created_at = timezone_aware_datetime()
+
+    # ForeignKeys
+    created_by_id: Mapped[Optional[int]] = mapped_column(ForeignKey("users.id"))
+    updated_by_id: Mapped[Optional[int]] = mapped_column(ForeignKey("users.id"))
 
     # Columns
     id: Mapped[int] = mapped_column(primary_key=True, index=True)
-    name: Mapped[str] = mapped_column(unique=True)
-    slug: Mapped[str] = mapped_column(unique=True)
+    name: Mapped[str] = mapped_column(unique=True, index=True)
+    description: Mapped[Optional[str]]
     active: Mapped[bool] = mapped_column(default=True)
+    created_at: Mapped[datetime]
+    updated_at: Mapped[Optional[datetime]] = mapped_column(
+        onupdate=lambda: timezone_aware_datetime()
+    )
 
     # Relationships
     menu_groups: Mapped[List["MenuGroup"]] = relationship(back_populates="category")
