e9aed3147378ff9155ac800b2859e94162aa0c1c
BaezFJ <baezdevs@gmail.com>
2025-07-12 13:26:13 -0500

    Refactor `MenuCategoryForm`: inherit from `BaseForm`, remove `submit` field, and simplify imports.

--- librepos/features/menu/forms/menu_category_form.py
+++ librepos/features/menu/forms/menu_category_form.py
@@ -1,19 +1,13 @@
-from flask_wtf import FlaskForm
-from wtforms import SubmitField, TextAreaField, BooleanField, StringField
+from wtforms import TextAreaField, BooleanField, StringField
 from wtforms.validators import DataRequired
 
+from librepos.common.forms import BaseForm
 from librepos.utils.form import default_placeholder, textarea_attributes
 
 
-class MenuCategoryForm(FlaskForm):
+class MenuCategoryForm(BaseForm):
     name = StringField(
         "Name", validators=[DataRequired()], render_kw=default_placeholder
     )
     description = TextAreaField("Description", render_kw=textarea_attributes)
     active = BooleanField("Active", default=True)
-    submit = SubmitField("Add Category")
-
-    def __init__(self, *args, submit_text=None, **kwargs):
-        super().__init__(*args, **kwargs)
-        if submit_text:
-            self.submit.label.text = submit_text
