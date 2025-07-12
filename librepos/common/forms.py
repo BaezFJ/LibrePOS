7c535ad9337b656d5817c62290f3b67002885ff3
BaezFJ <baezdevs@gmail.com>
2025-07-12 13:25:31 -0500

    Refactor forms: add `BaseForm` with configurable submit button, replace `ConfirmDeletionForm` with `ConfirmationForm`, and improve field handling.

--- librepos/common/forms.py
+++ librepos/common/forms.py
@@ -1,9 +1,22 @@
 from flask_wtf import FlaskForm
-from wtforms import SubmitField, StringField, HiddenField
-from wtforms.validators import DataRequired
+from wtforms import SubmitField, StringField
+from wtforms.validators import DataRequired, Length
 
 
-class ConfirmDeletionForm(FlaskForm):
-    id = HiddenField(validators=[DataRequired()])
-    confirmation = StringField("Type 'confirm' to delete", validators=[DataRequired()])
-    submit = SubmitField("Confirm Deletion", render_kw={"class": "btn danger"})
+class BaseForm(FlaskForm):
+    submit = SubmitField("Submit")
+
+    def __init__(self, *args, submit_text=None, **kwargs):
+        super().__init__(*args, **kwargs)
+        if submit_text:
+            self.submit.label.text = submit_text
+
+        # Move the submit field to the end
+        if 'submit' in self._fields:
+            submit_field = self._fields.pop('submit')
+            self._fields['submit'] = submit_field
+
+
+class ConfirmationForm(FlaskForm):
+    confirmation = StringField("Confirmation", validators=[DataRequired(), Length(min=6, max=15)])
+    submit = SubmitField("Confirm")
